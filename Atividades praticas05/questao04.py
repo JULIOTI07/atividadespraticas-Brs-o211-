2478b255b954d524276a015a451a0baf91200a7f
JULIOTI07 <junferr438@gmail.com>
2026-01-15 22:09:13 -0300

    Atividades praticas 05 - questão 04

---
+++ Atividades praticas05/questao04.py
@@ -0,0 +1,14 @@
+import datetime
+
+def calcular_idade(data_nascimento):
+    ano_atual = datetime.datetime.now().year
+    idade = ano_atual - ano_nascimento
+    return idade * 365
+
+ano_nascimento = int(input("Digite o ano de nascimento:"))
+
+idade_dias = calcular_idade(ano_nascimento)
+
+print(f"Você tem {idade_dias} dias de vida: ")
+
+
